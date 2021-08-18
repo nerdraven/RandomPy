aa0991a1e0b7a1ce4e33ebed07aa1978ebe552b2
Damian Akpan <damianakpan2001@gmail.com>
2021-08-18 16:29:46 +0100

    Worked on start_game service
    Signed-off-by: Damian Akpan <damianakpan2001@gmail.com>

--- src/domain/services.py
+++ src/domain/services.py
@@ -1,7 +1,10 @@
+from typing import Tuple
+from domain.entities import create_game, Player, Game
 
 
-def start_game():
-  pass
+def start_game(player: Player) -> Tuple[Game, bool]:
+  game = create_game(player)
+  return game, game.is_started()
 
 def play_move():
   pass
