71dc6bed6eefed1b141ce0d715d895916476075a
Damian Akpan <damianakpan2001@gmail.com>
2021-08-19 04:22:09 +0100

    Added redis config

--- src/app/config.py
+++ src/app/config.py
@@ -1,6 +1,11 @@
+import os
 
 class Config(object):
+  SECRET_KEY = os.environ.get("SECRET_KEY", None)
   SIZE = 4
+
+  REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+  REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 
 
 def get_config():
