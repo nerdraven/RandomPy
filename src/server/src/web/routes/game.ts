dfd24d3e94d20d7a97b5327ad28ac691c0f4e5c8
Damian Akpan <damianakpan2001@gmail.com>
2021-09-13 21:45:19 +0100

    Did some cleanup on the typings and logs
    Signed-off-by: Damian Akpan <damianakpan2001@gmail.com>

--- src/server/src/web/routes/game.ts
+++ src/server/src/web/routes/game.ts
@@ -1,16 +1,20 @@
-import { playMove, startGame } from "../../domain/services";
 import { Router } from "express";
+import { IConnect, IPlayGame } from "../../types";
 import { createPlayer } from "../../domain/entities";
-import { FakeGamePool } from "../../domain/repositories/gamePool";
+import { playMove, startGame } from "../../domain/services";
 import { FakeRepository } from "../../domain/repositories/game";
+import { FakeGamePool } from "../../domain/repositories/gamePool";
 
 const router = Router();
 const pool = new FakeGamePool();
 const repo = new FakeRepository();
 
 router.post("/start-game", (req, res) => {
-  const username: string = req.body.username;
-  const mainCode: string = req.body.mainCode;
+  const username: string = (<IConnect>req.body).userName;
+  const mainCode: string = (<IConnect>req.body).mainCode;
+
+  console.log("userName", username);
+  console.log("mainCode", mainCode);
 
   const player = createPlayer(username, mainCode);
   const [game, isStarted] = startGame([player], pool, repo);
@@ -19,11 +23,15 @@ router.post("/start-game", (req, res) => {
 });
 
 router.post("/play-move", (req, res) => {
-  const testCode: string = req.body.testCode;
-  const playerId: string = req.body.playerId;
-  const gameId: string = req.body.gameId;
+  const testCode: string = (<IPlayGame>req.body).testCode;
+  const userName: string = (<IPlayGame>req.body).userName;
+  const gameId: string = (<IPlayGame>req.body).gameId;
 
-  const result = playMove(testCode, playerId, gameId, repo);
+  console.log("userName", userName);
+  console.log("testCode", testCode);
+  console.log("gameId", gameId);
+
+  const result = playMove(testCode, userName, gameId, repo);
 
   res.json(result);
 });
